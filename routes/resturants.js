cdc4db01ba92d161f30ce85ccd72597e9bea2296
dsimmon1 <simmons.diana93@gmail.com>
2018-07-07 16:50:36 -0700

    added specific search parameters

--- routes/resturants.js
+++ routes/resturants.js
@@ -15,6 +15,21 @@ module.exports = function(app) {
             }
         });
     });
+    app.get("/api/nhresturants/:day", function(req, res) {
+        var choice = req.params.day;
+        console.log(choice);
+        Nhresturant.find({ [choice] : {$ne:"N/A"}}, function(error, found) {
+            // Log any errors
+            if (error) {
+                console.log(error);
+            }
+            // Otherwise, send json of the notes back to user
+            // This will fire off the success function of the ajax request
+            else {
+                res.json(found);
+            }
+        });
+    });
     app.post("/api/nhresturants", function(req, res) {
          Nhresturant.create(req.body, function(error, found) {
             // Log any errors
